192f29ffa3839bc4a7ee5b542c080434811b59d7
goodman99111 <goodman99111@gmail.com>
2020-04-23 23:00:31 +1000

    Update

--- Server/Engine/SetGetPosition.cs
+++ Server/Engine/SetGetPosition.cs
@@ -10,6 +10,7 @@ namespace WcfService.Engine
     public class SetGetPosition
     {
         private static string path = @"C:\Users\good\Desktop\Project\Game\Aggar\Console_Game\Server\App_Data\UserData\LastPosition.txt";
+        //Загрузка последней позиции
         public static Point LoadLastPosition(string login)
         {
             using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
@@ -29,31 +30,10 @@ namespace WcfService.Engine
                 return new Point(0,0);
             }
         }
+
+        //Сохранение последней позиции игрока
         public static void SaveLastPoint(string login, Point position)
         {
-                /*
-                string fullFile = String.Empty;
-                bool find = false;
-                string line;
-                while ((line = sr.ReadLine()) != null)
-                {
-                    string[] word = line.Split(' '); //разбиваем строку "логин позиция"
-                    if (word[1] == login)
-                    {
-                        word[2] = position.ToString();
-                        find = true;
-                    }
-                    fullFile += word[1] + " ";
-                    fullFile += word[2] + "\n";
-                }
-
-                if(!find)
-                {
-                    StreamWriter writer = new StreamWriter(path, true);
-                    writer.Write($"\n{login} {position.ToString()}");
-                    return;
-                }
-                */
                 string[] text = File.ReadAllLines(path);
                 for(int i = 0; i < text.Length; i++)
                 {
